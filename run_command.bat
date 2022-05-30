094355966ad8ff5b08c43ea4d6b1a4959e1debdf
tuanxhbk <nguyenmanhtuanxh@gmail.com>
2022-05-30 12:04:12 +0700

    Add comment

--- run_command.bat
+++ run_command.bat
@@ -1,5 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
+
+REM --- Read command from file ---
 set count=0
 for /f "tokens=*" %%x in (command.txt) do (
     set /a count+=1
@@ -9,10 +11,12 @@ REM echo %count%
 REM echo %var[10]%
 REM call %var[10]%
 
-set comment = ###
+REM --- Define comment out prefix ---
+set comment="###"
 
+REM --- Run command ---
 for /l %%n in (1,1,%count%) do (
-    Echo.!var[%%n]! | findstr /C:"###">nul || (
+    Echo.!var[%%n]! | findstr /C:%comment%>nul || (
     echo !var[%%n]!
     call !var[%%n]!
     )
