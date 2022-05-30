9199dbaf416bc5c36b259ee5e21b82fa678d4abe
tuanxhbk <nguyenmanhtuanxh@gmail.com>
2022-05-30 15:26:40 +0700

    Update

--- run_command.bat
+++ run_command.bat
@@ -1,11 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
 
+REM --- Set csv report folder ---
+set csv_folder="csv_report_%BUILD_NUMBER%"
+
 REM --- Read commands from file ---
 set count=0
 for /f "tokens=*" %%x in (command.txt) do (
     set /a count+=1
-    set var[!count!]=%%x
+    set var[!count!]=%%x + " --reporter-csv-export " + %csv_folder%
 )
 REM echo %count%
 REM echo %var[10]%
@@ -14,10 +17,8 @@ REM call %var[10]%
 REM --- Define comment out prefix ---
 set comment="###"
 
-set csv_folder="csv_report_%BUILD_NUMBER%"
-
 echo %csv_folder%
-mkdir -p %csv_folder%
+mkdir %csv_folder%
 
 REM --- Run command ---
 for /l %%n in (1,1,%count%) do (
